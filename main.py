c6b8e9981208e2ae757ddd37a0a730fd8f52bf4b
Nate Sales <nate@natesales.net>
2023-11-11 01:53:09 -0500

    feat: contour detection

--- main.py
+++ main.py
@@ -8,20 +8,22 @@ while True:
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     img = cv2.GaussianBlur(img, (3, 3), 0)
 
+    cv2.threshold(img, 127, 255, 0)
+
     # Edge detection
     edges = cv2.Canny(image=img, threshold1=100, threshold2=200)
-    cv2.imshow("edges", edges)
 
-    # Look for horizontal lines
-    lines = cv2.HoughLinesP(edges, 1, 3.14159 / 180, 100, minLineLength=100, maxLineGap=10)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # Look for contours
+    contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
+    cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
 
-    cv2.imshow("frame", frame)
+    for contour in contours:
+        contourX, contourY, contourW, contourH = cv2.boundingRect(contour)
+        contourMax = max(contourW, contourH)
+        cv2.putText(frame, str(contourMax), (contourX, contourY), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
+        cv2.drawContours(frame, contour, -1, (0, 255, 0), 1)
 
-    # TODO: Ignore anything outside page perimeter
+    cv2.imshow("img", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
