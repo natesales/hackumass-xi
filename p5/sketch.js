724a1c79d00eee3308d8d395a4fe09efc908928c
Hampton Moore <me@hamptonmoore.com>
2023-11-11 21:25:43 -0500

    frontend: sketch

--- p5/sketch.js
+++ p5/sketch.js
@@ -25,6 +25,18 @@ function raycast(p, dir, base) {
     if (p[1] >= SIZE[1]) {
       break;
     }
+
+    if (p[0] >= SIZE[0]) {
+      break;
+    }
+
+    if (p[1] < 0) {
+      break;
+    }
+
+    if (p[0] < 0) {
+      break;
+    }
   }
 
   return [p, count];
@@ -45,85 +57,11 @@ const player = {
     this.ay = 0;
     this.ax = 0;
 
-    const down = floorPoints([
-      [this.x, this.y + this.h],
-      [this.x + this.w/2, this.y + this.h],
-      [this.x + this.w, this.y + this.h],
-    ]).map((p) => {
-      const cast = raycast([...p], [0, 1], 255);
-      stroke("red");
-      strokeWeight(5);
-      point(...cast[0]);
-      return cast[1];
-    });
-
-    const up = floorPoints([
-      [this.x, this.y],
-      [this.x + this.w/2, this.y],
-      [this.x + this.w, this.y],
-    ]).map((p) => {
-      const cast = raycast([...p], [0, -1], 255);
-      stroke("magenta");
-      strokeWeight(5);
-      point(...cast[0]);
-      return cast[1];
-    });
-
-    const xMovement = this.vx < 0 ? -1 : this.vx > 0 ? 1 : 0;
-
-    if (xMovement != 0) {
-      const xDir = floorPoints([
-        [this.x + ((xMovement + 1) * this.w), this.y],
-        [this.x + ((xMovement + 1) * this.w), this.y + this.h/3],
-        [this.x + ((xMovement + 1) * this.w), this.y + this.h/2],
-        [this.x + ((xMovement + 1) * this.w), this.y + (2 * this.h)/3],
-        [this.x + ((xMovement + 1) * this.w), this.y + this.h],
-      ]).map((p) => {
-        const cast = raycast([...p], [xMovement, 0], 255);
-        stroke("blue");
-        strokeWeight(5);
-        point(...cast[0]);
-        return cast[1];
-      });
-
-      const minDir = min(...xDir);
-
-      if (minDir == 0) {
-        // this.vx *= 1 - (0.95 * dt)
-        this.vx = 0
-      }
-    }
-
-    const minDown = min(...down);
-
-    if (minDown > 1) {
-    } else {
-      this.vy = 0;
-    }
-
-    if (minDown == 1) {
-      if (this.vy > 0) {
-        this.vy = 0;
-      }
-      this.grounded = true;
-    } else if (minDown > 1) {
-      this.grounded = false;
-      this.ay += 128 * IN * dt;
-    } else if (minDown == 0) {
-      this.grounded = true;
-      const correction = min(...up);
-
-      if (correction < IN) {
-        this.y -= correction;
-
-        this.vx *= 1 / (1 + dt * (correction * 2));
-      }
-    }
-
     // Keyboard input
 
     if (KEYS[38] && this.grounded) {
-      this.vy -= 2 * IN;
+      this.vy -= 2.5 * IN;
+      this.grounded = false;
     }
 
     const AX = 156 * IN * dt;
@@ -174,15 +112,74 @@ const player = {
     if (this.grounded) {
       this.vx *= 1 / (1 + dt * 1.7);
     } else {
+      this.ay += 150 * IN * dt;
       this.vx *= 1 / (1 + dt * 1.4);
     }
 
-    this.vx = this.vx.clamp(-IN * 2, 2 * IN);
+    this.vx = this.vx.clamp(-IN * 2.5, 2.5 * IN);
 
     this.vx += this.ax * dt;
     this.vy += this.ay * dt;
     this.x += this.vx * dt;
     this.y += this.vy * dt;
+
+    // bound player to screen
+    this.x = this.x.clamp(5, SIZE[0] - this.w - 5);
+    this.y = this.y.clamp(5, SIZE[1] - this.h - 5);
+
+    // Collision detection
+    // Check bottom collision
+    this.grounded = false;
+    for (let i = 0; i < this.w; i++) {
+
+      // Check bottom
+      const color = get(this.x + i, this.y + this.h)[0];
+      if (color == 0) {
+        this.vy = 0;
+        
+        // find non black pixel
+        const [p, count] = raycast([this.x + i, this.y + this.h], [0, -1], 0);
+        this.y -= count;
+
+        this.grounded = true;
+        break;
+      }
+
+      // Check top
+      const color2 = get(this.x + i, this.y)[0];
+      if (color2 == 0) {
+        this.vy = 0;
+        const [p, count] = raycast([this.x + i, this.y], [0, 1], 0);
+        this.y += count;
+        break;
+      }
+    }
+
+    // Check left and right side collision, if collision is detected move player out of collision and set velocity to 0
+    for (let i = 0; i < this.h; i++) {
+
+      // Check left side
+      const color = get(this.x, this.y + i)[0];
+      if (color == 0) {
+        if (this.vx < 0) this.vx = 0;
+        const [p, count] = raycast([this.x, this.y + i], [1, 0], 0);
+        this.x += count;
+        break;
+      }
+
+      // Check right side
+      const color2 = get(this.x + this.w, this.y + i)[0];
+      if (color2 == 0) {
+        if (this.vx > 0) this.vx = 0;
+        const [p, count] = raycast([this.x + this.w, this.y + i], [-1, 0], 0);
+        this.x -= count;
+        break;
+      }
+    }
+
+    // bound player to screen
+    this.x = this.x.clamp(5, SIZE[0] - this.w - 5);
+    this.y = this.y.clamp(5, SIZE[1] - this.h - 5);
   },
   render: function () {
     image(skins[this.skinIndex], this.x, this.y, this.w, this.h);
@@ -304,14 +301,21 @@ function draw() {
     const dx = tl[0];
     const dy = tl[1];
 
-    const sx = w / width;
-    const sy = h / height;
+    const sx = w / SIZE[0];
+    const sy = h / SIZE[1];
 
     translate(dx, dy);
     scale(sx, sy);
   }
 
-  // renderLevel();
+  renderLevel();
+  // Border
+  stroke(0);
+  strokeWeight(IN / 8);
+  line(0, 0, SIZE[0], 0);
+  line(SIZE[0], 0, SIZE[0], SIZE[1]);
+  line(SIZE[0], SIZE[1], 0, SIZE[1]);
+  line(0, SIZE[1], 0, 0);
 
   player.render();
   resetMatrix();
